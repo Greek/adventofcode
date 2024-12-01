9d77c93ecaca775f7bf9085f9ac9a05cc63dc830
Andreas <git@fsck.rip>
2024-12-01 18:14:47 -0500

    problem 1: add pt1 and pt2
    Signed-off-by: Andreas <git@fsck.rip>

---
+++ 2024/1/pt1/solution.py
@@ -0,0 +1,25 @@
+def main():
+    location_ids1 = []
+    location_ids2 = []
+
+    total_distance = 0
+
+    with open("/Users/a/Documents/Code/adventofcode/2024/1/input.txt", "r") as input:
+        for line in input:
+            nums = line.replace("\n", "").split()
+
+            location_ids1.append(int(nums[0]))
+            location_ids2.append(int(nums[1]))
+
+    location_ids1.sort()
+    location_ids2.sort()
+
+    for i in range(0, len(location_ids1)):
+        total_distance += abs(location_ids1[i] - location_ids2[i])
+
+    return total_distance
+
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
---
+++ 2024/1/pt2/solution.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+def main():
+    location_ids1 = []
+    location_ids2 = []
+
+
+    total_distance = 0
+
+    with open(
+        "/Users/a/Documents/Code/adventofcode/2024/1/input.txt", "r"
+    ) as input:
+        for line in input:
+            nums = line.replace("\n", "").split()
+
+            location_ids1.append(int(nums[0]))
+            location_ids2.append(int(nums[1]))
+
+    occur = Counter(location_ids2)
+    similarity_score = sum(num * occur[num] for num in location_ids1)
+
+    for i in range(0, len(location_ids1)):
+        location_ids1.sort()
+        location_ids2.sort()
+
+        curr1 = location_ids1[i]
+        curr2 = location_ids2[i]
+
+        total_distance += abs(curr1 - curr2)
+
+    return total_distance, similarity_score
+
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
