32a72675f1ad0e13fa588b16537f8dd9f45e31ec
Andreas <git@fsck.rip>
2024-12-01 23:02:40 -0500

    problem 1: merge part 1 and part 2 into one file
    Signed-off-by: Andreas <git@fsck.rip>

--- 2024/1/solution.py
+++ 2024/1/solution.py
@@ -1,7 +1,9 @@
 import os
+from collections import Counter
 
 
 def main():
+    # Part 1
     location_ids1 = []
     location_ids2 = []
 
@@ -20,7 +22,11 @@ def main():
     for i in range(len(location_ids1)):
         total_distance += abs(location_ids1[i] - location_ids2[i])
 
-    return total_distance
+    # Part 2
+    counts = Counter(location_ids2)
+    similarity_score = sum(num * counts[num] for num in location_ids1)
+
+    return total_distance, similarity_score
 
 
 if __name__ == "__main__":
